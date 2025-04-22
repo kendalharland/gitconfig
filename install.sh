76e332c1511f0c86a4b3541bafdb56b542a5df56
Kendal Harland <kendal@thebrowser.company>
2025-04-22 14:36:14 -0700

    Backup alias before reinstall

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
-#!/bin/sh
-
-# Patch gitconfig 
+# Patch gitconfig
 cat gitconfig >> ~/.gitconfig
 
-# Install aliasas
-mkdir -p ~/.config/gitconfig
-cp -r aliases ~/.config/gitconfig
+# Backup and install aliases
+ALIAS_DIR="$HOME/.config/gitconfig/aliases"
+ALIAS_BACKUP="$HOME/.config/gitconfig_backup_$(date +%Y%m%d_%H%M%S).zip"
+
+if [ -d "$ALIAS_DIR" ] && [ "$(ls -A "$ALIAS_DIR")" ]; then
+  echo "Backing up existing aliases to $ALIAS_BACKUP"
+  zip -r "$ALIAS_BACKUP" "$ALIAS_DIR"
+  rm -rf "$ALIAS_DIR"
+fi
+
+mkdir -p "$ALIAS_DIR"
+cp -r aliases/* "$ALIAS_DIR"
 
 # Test
 git alias
-
